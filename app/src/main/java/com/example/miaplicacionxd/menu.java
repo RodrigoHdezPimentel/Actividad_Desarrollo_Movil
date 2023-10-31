03c34878f9acbedaed6c1e0158fe22c2deb71e2e
Rodrigo <rodrigo.hdez.oficial@gmail.com>
2023-10-31 13:02:18 +0100

    Menu in progress, login needs review

--- app/src/main/java/com/example/miaplicacionxd/menu.java
+++ app/src/main/java/com/example/miaplicacionxd/menu.java
@@ -1,8 +1,14 @@
 package com.example.miaplicacionxd;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class menu extends AppCompatActivity {
 
@@ -10,5 +16,28 @@ public class menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        TextView ClickToStart = findViewById(R.id.startbutton);
+        ClickToStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToGame();
+            }
+        });
+
+        RelativeLayout Fondo = findViewById(R.id.Fondo);
+        ImageView profileimg = findViewById(R.id.ProfileImg);
+        profileimg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ClickToStart.setEnabled(false);
+                profileimg.setEnabled(false);
+                Fondo.setBackground(getDrawable(R.color.background_light_green));
+            }
+        });
+    }
+    public void goToGame(){
+        Intent intentToGame = new Intent(menu.this, game.class);
+        startActivity(intentToGame);
     }
 }
